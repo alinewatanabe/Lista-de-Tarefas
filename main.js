c676cea2e5c65f4eeba2bdf8eeed0a659217f3bc
Aline <84539092+alinewatanabe@users.noreply.github.com>
2023-02-09 21:27:22 -0300

    Remove a ultima tarefa

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function addNewTask() {
     if(text.length === 0) {
         alert("Tarefa precisa de ter mais de 1 caracter");
         return;
-        
     }
 
     var listItem = document.createElement("li");
@@ -17,9 +16,6 @@ function addNewTask() {
 }
 
 function removeTask() {
-    var list = document.getElementById("list");
-    var text = document.getElementById("task_name").value;
-
-    Element.remove(list);   
-
+    var task = document.querySelector(".list-item")
+    task.remove();
 }
